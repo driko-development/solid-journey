b885a10213b2e76a48039de9acf2616ae4bb9a3b
Aguilera <edrik.aguilera@mavs.uta.edu>
2021-03-03 21:02:25 -0600

    Initial upload for libc attack

--- ReturnToLibC/retlib.c
+++ ReturnToLibC/retlib.c
@@ -1,6 +1,3 @@
-/* Vunlerable program: stack.c */
-/* You can get this program from the lab's website */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,33 +5,34 @@
 /* Changing this size will change the layout of the stack.
  * Instructors can change this value each year, so students
  * won't be able to use the solutions from the past.
- * Suggested value: between 0 and 400  */
+ * Suggested value: between 0 and 200 (cannot exceed 300, or
+ * the program won't have a buffer-overflow problem). */
 #ifndef BUF_SIZE
-#define BUF_SIZE 24
+#define BUF_SIZE 12
 #endif
 
-int bof(char *str)
+int bof(FILE *badfile)
 {
     char buffer[BUF_SIZE];
 
     /* The following statement has a buffer overflow problem */
-    strcpy(buffer, str);       
+    fread(buffer, sizeof(char), 300, badfile);
 
     return 1;
 }
 
 int main(int argc, char **argv)
 {
-    char str[517];
     FILE *badfile;
 
-     /* Change the size of the dummy array to randomize the parameters
+    /* Change the size of the dummy array to randomize the parameters
        for this lab. Need to use the array at least once */
-    char dummy[BUF_SIZE];  memset(dummy, 0, BUF_SIZE);
+    char dummy[BUF_SIZE*5];  memset(dummy, 0, BUF_SIZE*5);
 
     badfile = fopen("badfile", "r");
-    fread(str, sizeof(char), 517, badfile);
-    bof(str);
+    bof(badfile);
+
     printf("Returned Properly\n");
+    fclose(badfile);
     return 1;
 }
